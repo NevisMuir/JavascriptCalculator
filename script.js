8be8d42aa4054359716d5a3c49c9ddc9a773a2af
NevisMuir <rmuir310@gmail.com>
2022-08-09 17:56:53 -0400

    Clarify variable names

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let display=document.querySelector('#storedNum')
-//indicator for if activeNum is a result of an operation
+let operationScreen=document.querySelector('#operationScreen')
+//indicator for if mainScreen is a result of an operation
 let indicator=false
-//activeNum is where we print the numbers and results
-let activeNum = document.querySelector('#activeNum')
-activeNum.innerHTML=''
+//mainScreen is where we print the numbers and results
+let mainScreen = document.querySelector('#mainScreen')
+mainScreen.innerHTML=''
 //this array will store numbers and results
 let array = []
 
@@ -11,8 +11,8 @@ let array = []
 let AC=document.querySelector('#AC')
 AC.addEventListener('click',clear)
 function clear(){
-   display.innerHTML=''
-   activeNum.innerHTML=''
+   operationScreen.innerHTML=''
+   mainScreen.innerHTML=''
     array.splice(0);
 }
 
@@ -37,28 +37,30 @@ function operate (array){
     if(array[1] === '/'){return divide(array)}
 }
 function answer(){
-    array.push(activeNum.innerHTML)
+    //adds our new number to the end of the array for operation
+    array.push(mainScreen.innerHTML)
     let answer=operate(array)
-    display.innerHTML=''
-    activeNum.innerHTML=answer
-    array.splice(0,3,activeNum.innerHTML)
+    operationScreen.innerHTML=''
+    //puts our new answer on our main screen, and makes our answer new number`
+    mainScreen.innerHTML=answer
+    array.splice(0,3,mainScreen.innerHTML)
     indicator=true
 }
 
 //enter function
 const enter=document.querySelector('#enter')
 enter.addEventListener('click', ()=>{
-    if(array.length==2&&activeNum.innerHTML!=='')answer()
+    if(array.length==2&&mainScreen.innerHTML!=='')answer()
 })
 //backspace function
 const backspace=document.querySelector('#backspace')
 backspace.addEventListener('click', ()=>{
-    if(activeNum.innerHTML!=='' && indicator==false){
-        let newString=activeNum.innerHTML.slice(0,-1)
-        activeNum.innerHTML=newString
+    if(mainScreen.innerHTML!=='' && indicator==false){
+        let newString=mainScreen.innerHTML.slice(0,-1)
+        mainScreen.innerHTML=newString
     }
 })
-//number buttons print number into activeNum, saved in currentNumber
+//number buttons print number into mainScreen, saved in currentNumber
 const numButtons = document.querySelectorAll(".num");
 numButtons.forEach((numButton)=>{
     numButton.addEventListener('click', ()=>{
@@ -69,17 +71,18 @@ numButtons.forEach((numButton)=>{
         }
         //keeps progress if operator already pressed
         if(indicator==true && array.length==2){
-            display.innerHTML=array[0] + ' ' + array[1]
-            activeNum.innerHTML=''
+            operationScreen.innerHTML=array[0] + ' ' + array[1]
+            mainScreen.innerHTML=''
             indicator=false 
         }
         //prevents multiple decimal points
-        if(activeNum.innerHTML.includes(".")&&numButton.innerHTML=='.')return
+        if(mainScreen.innerHTML.includes(".")&&numButton.innerHTML=='.')return
         //prevents multiple zeroes and numbers with more than 8 digits
-        if(activeNum.innerHTML.length>=8) return;
+        if(mainScreen.innerHTML.length>=8) return;
         //prevents double zeroes
-        if(activeNum.innerHTML=='0')activeNum.innerHTML=''
-        activeNum.innerHTML += numButton.innerHTML
+        if(mainScreen.innerHTML=='0')mainScreen.innerHTML=''
+        //prints number on screen
+        mainScreen.innerHTML += numButton.innerHTML
     })
 })
 //operator press function
@@ -89,28 +92,28 @@ operators.forEach((operator)=> {
         //start from fresh if result still stands
         if(indicator===true){
             array.splice(1,1,operator.id)
-            activeNum.innerHTML=''
+            mainScreen.innerHTML=''
             indicator=false
-            display.innerHTML=array[0] + ' ' + array[1]
+            operationScreen.innerHTML=array[0] + ' ' + array[1]
             return
         }
         //ignores if no input
-        if(array.length==0 && activeNum.innerHTML=='') return
+        if(array.length==0 && mainScreen.innerHTML=='') return
         //enters first value if none
         if(array.length==0){
-            array.push(Number(activeNum.innerHTML),operator.id)
-            activeNum.innerHTML=''
+            array.push(Number(mainScreen.innerHTML),operator.id)
+            mainScreen.innerHTML=''
         }
         //change operator before entering new number
-        if(array.length==2&&activeNum.innerHTML=='')array.splice(1,1,operator.id)
+        if(array.length==2&&mainScreen.innerHTML=='')array.splice(1,1,operator.id)
         //operate
-        if(array.length==2&&activeNum.innerHTML!==''){
+        if(array.length==2&&mainScreen.innerHTML!==''){
             answer()
             array.push(operator.id)
             return
         }
         //publishes operation in progress to screen
-        display.innerHTML=array[0] + ' ' + array[1]
+        operationScreen.innerHTML=array[0] + ' ' + array[1]
    
     })
 })
