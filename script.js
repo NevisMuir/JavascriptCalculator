d1db081447076134adaef4564d992b9775a17516
NevisMuir <rmuir310@gmail.com>
2022-08-09 17:38:41 -0400

    Redo Javascript, operation progress in screen Started from scratch and did js completely differently. All buttons function as intended. Still need to go through and simplify code, and limit character length of results.

--- script.js
+++ script.js
@@ -1,73 +1,114 @@
+let display=document.querySelector('#storedNum')
+function clear(){
+   display.innerHTML=''
+   activeNum.innerHTML=''
+    array.splice(0);
+}
+
+let AC=document.querySelector('#AC')
+AC.addEventListener('click',clear)
+
 function add (array){
-    return array[0]+array[1]
+    return Number(array[0])+Number(array[2])
 }
 function subtract(array){
-    return array[0]-array[1]
+    return Number(array[0])-Number(array[2])
 }
-function multiply (array){
-    
-    return array[0]*array[1]
+function multiply (array){ 
+    return Number(array[0])*Number(array[2])
 }
 function divide (array){
-    return array[0]/array[1]
+    return Number(array[0])/Number(array[2])
 }
 
 function operate (array){
-    if(currentOperator === '+'){return add(array)}
-    if(currentOperator === '-'){return subtract(array)}
-    if(currentOperator === '*'){return multiply(array)}
-    if(currentOperator === '/'){return divide(array)}
+    if(array[1] === '+'){return add(array)}
+    if(array[1] === '-'){return subtract(array)}
+    if(array[1] === '*'){return multiply(array)}
+    if(array[1] === '/'){return divide(array)}
 }
-
+function answer(){
+    array.push(activeNum.innerHTML)
+    let answer=operate(array)
+    display.innerHTML=''
+    activeNum.innerHTML=answer
+    array.splice(0,3,activeNum.innerHTML)
+    indicator=true
+}
+let indicator=false
 //activeNum is where we print the numbers and results
 let activeNum = document.querySelector('#activeNum')
-activeNum.innerHTML=0
-let currentNumber='0'
-let currentOperator=''
+activeNum.innerHTML=''
 //this array will store numbers and results
-let numberArray = []
+let array = []
+//enter function
+const enter=document.querySelector('#enter')
+enter.addEventListener('click', ()=>{
+    if(array.length==2&&activeNum.innerHTML!=='')answer()
+})
 
-
+const backspace=document.querySelector('#backspace')
+backspace.addEventListener('click', ()=>{
+    if(activeNum.innerHTML!=='' && indicator==false){
+        let newString=activeNum.innerHTML.slice(0,-1)
+        activeNum.innerHTML=newString
+    }
+})
 //number buttons print number into activeNum, saved in currentNumber
 const numButtons = document.querySelectorAll(".num");
 numButtons.forEach((numButton)=>{
     numButton.addEventListener('click', ()=>{
+        //clears field if no previous operator
+        if(indicator==true && array.length<2){
+            clear()
+            indicator=false
+        }
+        //keeps progress if operator already pressed
+        if(indicator==true && array.length==2){
+            display.innerHTML=array[0] + ' ' + array[1]
+            activeNum.innerHTML=''
+            indicator=false 
+        }
+        //prevents multiple decimal points
         if(activeNum.innerHTML.includes(".")&&numButton.innerHTML=='.')return
-        if(activeNum.innerHTML==0 && numButton.innerHTML==0||currentNumber.length>=8) return;
-        if(activeNum.innerHTML==0) currentNumber=""
-        currentNumber += numButton.innerHTML
-        activeNum.innerHTML = currentNumber
+        //prevents multiple zeroes and numbers with more than 8 digits
+        if(activeNum.innerHTML.length>=8) return;
+        //prevents double zeroes
+        if(activeNum.innerHTML=='0')activeNum.innerHTML=''
+        activeNum.innerHTML += numButton.innerHTML
     })
 })
+//operator press function
 const operators = document.querySelectorAll(".operator");
 operators.forEach((operator)=> {
     operator.addEventListener('click', ()=> {
-        //move current number into end of array
-        numberArray.push(Number(activeNum.innerHTML));
-        //if array is over 3, cut first value
-        if (numberArray.length>2)numberArray.shift();
-        currentNumber=''
-        //if current operator is empty,  save and await second number
-        if (currentOperator == ''|| currentOperator=='enter'){currentOperator=operator.id
-        return}
-        //if current operator is not empty, run operate and return to activeNum
-        if(currentOperator !==''){
-            let result = operate(numberArray)
-            currentOperator=operator.id            
-            numberArray.push(result)
-            if (numberArray.length > 2)numberArray.shift()
-            activeNum.innerHTML=result
+        //start from fresh if result still stands
+        if(indicator===true){
+            array.splice(1,1,operator.id)
+            activeNum.innerHTML=''
+            indicator=false
+            display.innerHTML=array[0] + ' ' + array[1]
+            return
         }
+        //ignores if no input
+        if(array.length==0 && activeNum.innerHTML=='') return
+        //enters first value if none
+        if(array.length==0){
+            array.push(Number(activeNum.innerHTML),operator.id)
+            activeNum.innerHTML=''
+        }
+        //change operator before entering new number
+        if(array.length==2&&activeNum.innerHTML=='')array.splice(1,1,operator.id)
+        if(array.length==2&&activeNum.innerHTML!==''){
+            answer()
+            array.push(operator.id)
+            return
+        }
+        display.innerHTML=array[0] + ' ' + array[1]
+   
     })
 })
-const clear = document.querySelector('#AC')
-clear.addEventListener('click', ()=>{
-    activeNum.innerHTML=0
-    currentNumber='0'
-    currentOperator=''
-    numberArray = []
-})
-const backspace = document.querySelector("#backspace")
-backspace.addEventListener('click',()=>{
-    activeNum.innerHTML = currentNumber.slice(0,-1)
-})
+``
+
+
+
